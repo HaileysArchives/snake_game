4e9709cdf0537d18bc6603d685f1d5b2d5eeb378
Hailey <carly1982@naver.com>
2023-07-18 13:49:08 +0900

    Eighth commit
    Create a food.py (Inheritance turtle class) => add __init__(), refresh() function.
    And from food import Food in main.py. => #Detect collision with food.
    
    if snake.head.distance(food) < 15: => Newly Learned Functions : distance()

---
+++ food.py
@@ -0,0 +1,21 @@
+# create new food.py
+
+from turtle import Turtle, Screen
+import random
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len = 0.5, stretch_wid=0.5) # 10x10 원래는 20x20
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 from snake import Snake 
+from food import Food 
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen() 
 screen.onkey(snake.up, "Up") # angle: 90
@@ -23,3 +25,6 @@ while game_is_on:
     time.sleep(0.1) 
 
     snake.move()
+
+    if snake.head.distance(food) < 15: # distance()
+        food.refresh()
